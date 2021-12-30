e3b47a433c88da39d3a294064ca0c608f6c17687
Marcel Herhold <www.marcel.herhold@googlemail.com>
2021-12-30 20:34:04 +0100

    Edit script

--- scripts/createItems.sh
+++ scripts/createItems.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-for i in {1..20} ; do
-  curl -H "Content-Type: application/json" -d '{"name": "testItem-'"$i"'", "amount": "500g"}' "localhost:9000/item"
+for i in {5..405} ; do
+  curl -H "Content-Type: application/json" -d '{"name": "testItem-'"$i"'", "amount": '"$i"'}' "localhost:9000/item"
 done
